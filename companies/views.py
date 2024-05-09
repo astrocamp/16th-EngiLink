0ba774b8acb4d97af5654b27a8aa0209a66b7875
JeffreyWu <wu19930628@gmail.com>
2024-05-09 15:59:06 +0800

    add companies css style

--- companies/views.py
+++ companies/views.py
@@ -2,14 +2,19 @@ from .forms import CompanyUserCreationForm, CompanyUserChangeForm
 from django.urls import reverse_lazy, reverse
 from django.views.generic import TemplateView, UpdateView, DetailView
 from django.views.generic.edit import CreateView
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+from django.contrib import messages
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Company
 
 class LogOutView(LogoutView):
     template_name = "companies/logout.html"
-
+    
+    def dispatch(self, request, *args, **kwargs):
+        messages.success(request, "登出成功!")
+        logout(request)
+        return super().dispatch(request, *args, **kwargs)
 
 class LogInView(LoginView):
     template_name = "companies/login.html"
@@ -17,7 +22,20 @@ class LogInView(LoginView):
     def get_success_url(self):
         url = self.get_redirect_url()
         return reverse("index") 
-
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.method == 'POST' and not self.request.POST.get('username') and not self.request.POST.get('password'):
+            messages.info(self.request, "請輸入使用者代號與密碼!")
+        return context
+    
+    def form_valid(self, form):
+        messages.success(self.request, "登入成功!")
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.error(self.request, "登入失敗, 請確認輸入的訊息!")
+        return self.render_to_response(self.get_context_data(form=form))
 class IndexView(TemplateView):
     template_name = "companies/index.html"
 
@@ -29,6 +47,7 @@ class SignUpView(CreateView):
 
     def form_valid(self, form):
         response = super().form_valid(form)
+        messages.success(self.request, "註冊成功!")
         login(self.request, self.object, backend='django.contrib.auth.backends.ModelBackend')
         return response
     
