0f78fc75ee00ec4b8331227b11910faf89db2fbe
JeffreyWu <wu19930628@gmail.com>
2024-05-16 21:09:00 +0800

    merge migration file

--- scripts/drag.js
+++ scripts/drag.js
@@ -7,18 +7,26 @@ document.addEventListener('DOMContentLoaded', () => {
         draggable: 'p',
     });
     sortable.on('drag:start', (evt) => {
+        // console.log('START')
+        // console.log(evt)
         evt.data.source.classList.add('draggable--is-dragging');
     });
 
     sortable.on('drag:stop', (evt) => {
+        console.log('STOP')
+        console.log(evt)
         evt.data.source.classList.remove('draggable--is-dragging');
     });
 
     sortable.on('sortable:sort', (evt) => {
+        // console.log('SORT')
+        // console.log(evt)
         evt.data.dragEvent.source.classList.add('draggable--is-over');
     });
 
     sortable.on('sortable:sorted', (evt) => {
+        console.log('SORTED')
+        console.log(evt)
         evt.data.dragEvent.source.classList.remove('draggable--is-over');
     });
     }
